ae895b6e68a858c31643fb1760af8b2e8169362c
AlexKhuel <160157080+AlexKhuel@users.noreply.github.com>
2025-03-27 13:44:48 -0500

    Create hasTeen.java

---
+++ Warmup-1/hasTeen.java
@@ -0,0 +1,6 @@
+public boolean hasTeen(int a, int b, int c) {
+  if(a <= 19 && a >= 13) return true;
+  if(b <= 19 && b >= 13) return true;
+  if(c <= 19 && c >= 13) return true;
+  return false;
+}
